4ba7f30ea97a5e3cbcca60508f4d0948259588c0
Szymek131 <sz.wiktor131@gmail.com>
2023-04-26 15:28:17 +0200

    updated form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Footer from "./components/views/Footer/Footer";
 import Header from "./components/views/Header/Header";
 import Home from "./components/pages/Home/Home";
+import NotFound from "./components/pages/NotFound/NotFound";
 import TableDetails from "./components/features/TableDetails/TableDetails";
 import { fetchTables } from "./redux/tablesRedux";
 import { useDispatch } from "react-redux";
@@ -19,6 +20,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/table/detail/:id' element={<TableDetails />} />
+        <Route path='/*' element={<NotFound />} />
       </Routes>
       <Footer />
     </Container>
--- src/components/features/TableDetails/TableDetails.js
+++ src/components/features/TableDetails/TableDetails.js
@@ -1,17 +1,60 @@
+import styles from './TableDetails.module.scss';
 import { useParams, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { getTableById } from "../../../redux/tablesRedux";
+import { Form, Button } from "react-bootstrap"
+import { useState, useEffect } from 'react';
+import clsx from 'clsx';
 
 const TableDetails = () => {
   const { id } = useParams();
+  const [tableStatus, setTableStatus] = useState('');
+  const [bill, setBill] = useState(0);
+  const [minPeopleAmount, setMinPeopleAmount] = useState(0);
+  const [maxPeopleAmount, setMaxPeopleAmount] = useState(0);
   const table = useSelector(state => getTableById(state, id));
 
-
   if (!table) return <Navigate to='/' />
+  setTableStatus(table.status)
 
+  const statuses = ['Busy', 'Cleaning', 'Free', 'Reserved'];
+  const newStatuses = statuses.filter(word => word !== table.status)
+  const shouldDisplay = (tableStatus === 'Busy' && styles.statusDisplay) && (table.status === 'Busy' && styles.statusDisplay);
+  setTableStatus(table.status);
   return (
     <div>
-      {table.status}
+      <Form>
+        <p className={styles.tableName}>Table {table.id}</p>
+        <div className={styles.singleFormContainer}>
+          <p className={styles.formCategories}>Status: </p>
+          <div className={styles.select}>
+            <Form.Select onChange={e => setTableStatus(e.target.value)}>
+              <option>{table.status}</option>
+              {newStatuses.map(status => (
+                <option>{status}</option>
+              ))};
+            </Form.Select>
+          </div>
+        </div>
+        <div className={clsx(styles.singleFormContainer, shouldDisplay)}>
+          <p className={styles.formCategories}>People: </p>
+          <div className={styles.peopleInput}>
+            <Form.Control ></Form.Control>
+          </div>
+          <span className={styles.slash}>/</span>
+          <div className={styles.peopleInput}>
+            <Form.Control ></Form.Control>
+          </div>
+        </div>
+        <div className={clsx(styles.singleFormContainer, shouldDisplay)}>
+          <p className={styles.formCategories}>Bill: </p>
+          <span className={styles.dolarSing}>$</span>
+          <div className={styles.billInput}>
+            <Form.Control value={table.bill} />
+          </div>
+        </div>
+        <Button className={styles.updateButton} type="submit" variant="primary" size="lg" >Update</Button>
+      </Form>
     </div>
   )
 };
